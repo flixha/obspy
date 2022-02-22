e350dffbc5082dbfeed368ec3785c001b0341600
flixha <felix.halpaap@uib.no>
2022-02-22 16:41:27 +0100

    some fixes for handling Coda and app. vel

--- obspy/io/nordic/core.py
+++ obspy/io/nordic/core.py
@@ -2155,6 +2155,8 @@ def nordpick(event, high_accuracy=True, nordic_format='OLD'):
                     peri = ' '
                     peri_round = False
                     amp = None
+                    coda_eval_mode = INV_EVALUTATION_MAPPING.get(
+                        amplitude.evaluation_mode, None)
                 if nordic_format == 'OLD':  # only use 1st amplitude
                     break
                 amp_list.append(amp)
@@ -2323,8 +2325,9 @@ def nordpick(event, high_accuracy=True, nordic_format='OLD'):
                     tr_mag = [
                         sta_mag for sta_mag in event.station_magnitudes
                         if (sta_mag.amplitude_id == amplitudes[j].resource_id
-                            and sta_mag.creation_info.agency_id
-                            == pick.creation_info.agency_id
+                            and (not sta_mag.creation_info or
+                                 sta_mag.creation_info.agency_id
+                                 == pick.creation_info.agency_id)
                             and sta_mag.station_magnitude_type
                             == amplitudes[j].magnitude_hint)]
                     amp_residuals.append('     ')
@@ -2364,6 +2367,19 @@ def nordpick(event, high_accuracy=True, nordic_format='OLD'):
                     ain=ain.rjust(5)[0:5], residual=residual,
                     finalweight=finalweight,
                     distance=distance.rjust(5)[0:5],
+                    caz=_str_conv(caz).rjust(3)[0:3]))
+            if add_coda_line:
+                pick_strings.append(pick_string_formatter.format(
+                    station=pick.waveform_id.station_code,
+                    channel=channel_code, network=network_code,
+                    location=location_code, impulsivity=' ',
+                    phase_hint=coda_phase_hint.ljust(8)[0:8],
+                    weight=' ', eval_mode=coda_eval_mode,
+                    hour=pick_hour, minute=pick.time.minute,
+                    seconds=_str_conv(pick_seconds, rounded=3).rjust(6),
+                    par1=coda_par1, par2=coda_par2, agency=agency,
+                    author=author, ain='     ', residual=coda_residual,
+                    finalweight=' ', distance=distance.rjust(5)[0:5],
                     caz=_str_conv(caz).rjust(3)[0:3]))
             if add_amp_line:
                 for j, amp in enumerate(amp_list):
